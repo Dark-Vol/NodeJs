1996fd7b90588dda114084f348dc7fbf209a4715
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2024-10-07 14:09:02 +0300

    add new rulse

---
+++ lesson7/practic/controllers/postsController.js
@@ -0,0 +1,16 @@
+
+
+
+class PostsController {
+    static async getAll(req,resp) {
+        return resp.status(200).json({message:"GET ALL OK"})
+    }
+    
+    static async create(req,resp) {
+        console.log(req.body)
+        return resp.status(201).json({message:"CREATE OK"})
+    }
+}
+
+
+module.exports = PostsController
